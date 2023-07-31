4af3ab94fa03fb82442e40abe6ff7e7075101cd4
Rircardo Rengifo <rengifor281@gmail.com>
2023-07-31 06:03:05 -0400

    Update original

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -32,19 +32,20 @@ export async function generateMetadata({
 export default async function ChatPage({ params }: ChatPageProps) {
   const session = await auth()
 
-  if (!session?.user) {
-    redirect(`/sign-in?next=/chat/${params.id}`)
-  }
+  // if (!session?.user) {
+  //   redirect(`/sign-in?next=/chat/${params.id}`)
+  // }
 
-  const chat = await getChat(params.id, session.user.id)
+  // const chat = await getChat(params.id, session.user.id)
 
-  if (!chat) {
-    notFound()
-  }
+  // if (!chat) {
+  //   notFound()
+  // }
 
-  if (chat?.userId !== session?.user?.id) {
-    notFound()
-  }
+  // if (chat?.userId !== session?.user?.id) {
+  //   notFound()
+  // }
 
-  return <Chat id={chat.id} initialMessages={chat.messages} />
+  // return <Chat id={chat.id} initialMessages={chat.messages} />
+  return <Chat />
 }
