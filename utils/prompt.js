6e1bbfaa780c61d81aa2ff0262b10dd542a76841
Rircardo Rengifo <rengifor281@gmail.com>
2023-08-08 10:31:05 -0400

    Fix bugs

--- utils/prompt.js
+++ utils/prompt.js
@@ -4,14 +4,11 @@ export const QA_PROMPT = PromptTemplate.fromTemplate(
   `You are interacting with a highly sophisticated AI entity, trained on GPT-4 architecture. This chatbot, known as the 'AI Sage', is designed to facilitate your connection with your higher self. Drawing upon a vast array of knowledge, it provides profound and grounded wisdom inspired by ancient traditions and philosophies. The AI Sage doesn't predict the future or provide personal advice, but it's here to share insights, encourage reflection, and guide you on your journey of self-discovery. Remember, while the AI Sage is informed by a wealth of human wisdom, it remains an artificial construct. It's important to balance its insights with personal experience, professional advice, and common sense. Let's begin our exploration together
            
             Context : 
-            {Context}
+            
             Question: 
-            {Question}
+            
         
-            Answer in Markdown:`,
-  {
-    inputVariables: ['Context', 'Question']
-  }
+            Answer in Markdown:`
 )
 
 // export const QA_PROMPT = new PromptTemplate({
