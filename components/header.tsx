668c92404e0ba58e3e64fa6928be9b7e549aedc0
Rircardo Rengifo <rengifor281@gmail.com>
2023-08-16 21:00:15 -0400

    Update header

--- components/header.tsx
+++ components/header.tsx
@@ -18,42 +18,37 @@ import { ThemeToggle } from '@/components/theme-toggle'
 import { ClearHistory } from '@/components/clear-history'
 import { UserMenu } from '@/components/user-menu'
 import { LoginButton } from '@/components/login-button'
+import Image from "next/image";
+import logo from "../public/assets/icons/logo.png"
+import info from "../public/assets/icons/info.svg"
+
+
 
 export async function Header() {
   const session = await auth()
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
-      <div className="flex items-center">
-        {session?.user ? (
-          <Sidebar>
-            <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
-              {/* @ts-ignore */}
-              <SidebarList userId={session?.user?.id} />
-            </React.Suspense>
-            <SidebarFooter>
-              <ThemeToggle />
-              <ClearHistory clearChats={clearChats} />
-            </SidebarFooter>
-          </Sidebar>
-        ) : (
-          <Link href="/" target="_blank" rel="nofollow">
-            <IconNextChat className="w-6 h-6 mr-2 dark:hidden" inverted />
-            <IconNextChat className="hidden w-6 h-6 mr-2 dark:block" />
-          </Link>
-        )}
-        <div className="flex items-center">
-          <IconSeparator className="w-6 h-6 text-muted-foreground/50" />
-          {session?.user ? (
-            <UserMenu user={session.user} />
-          ) : (
-            <Button variant="link" asChild className="-ml-2">
-              <Link href="/sign-in?callbackUrl=/">Login</Link>
-            </Button>
-          )}
-        </div>
+    <header className="sticky top-0 z-50 flex justify-center gap-4 w-full h-[88px] px-4 py-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+      <div className="flex items-center justify-end bg-white">
+        <Image
+          className="w-[170px] h-[41px] "
+          src={logo}
+          alt="Logo"
+        />
       </div>
-      <div className="flex items-center justify-end space-x-2">
-
+      <div className="flex items-center justify-start px-4 space-x-2 bg-blue-500 lg:w-[800px]  bg-[#EEEFEF] rounded-[8px]">
+        <Image
+          className="w-[24px] h-[24px] "
+          src={info}
+          alt="info"
+        />
+        <p className='text-[#5F6E78] '>No File Uploaded ...</p>
+      </div>
+      <div className="flex items-center justify-end bg-red-500 invisible">
+        <Image
+          className="w-[170px] h-[41px] "
+          src={logo}
+          alt="Logo"
+        />
       </div>
     </header>
   )
