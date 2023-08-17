e06d2b452e29b6d74e1a0857622e4da831eeab3d
Rircardo Rengifo <rengifor281@gmail.com>
2023-08-17 02:13:57 -0400

    Fix conflict

--- components/header.tsx
+++ components/header.tsx
@@ -27,7 +27,7 @@ import info from "../public/assets/icons/info.svg"
 export async function Header() {
   const session = await auth()
   return (
-    <header className="sticky top-0 z-50 flex flex-col md:flex-row items-center justify-center md:gap-4 gap-0 w-full h-[88px] h-auto md:px-4 md:py-4 pb-[0px] border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex flex-col md:flex-row items-center justify-center md:gap-4 gap-0 w-full  h-auto md:px-4 md:py-4 pb-[0px] border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center justify-end bg-white p-2">
         <Image
           className="w-[170px] h-[41px] "
